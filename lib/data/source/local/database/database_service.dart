9bf811a07bd3edb5449f8b6aff642c8cc9ddee2e
ujjwalkarkiEB <ujjwal.karki@ebpearls.com>
2024-06-25 16:30:53 +0545

    fix: dbservice abstract implementation

--- lib/data/source/local/database/database_service.dart
+++ lib/data/source/local/database/database_service.dart
@@ -1,10 +1,6 @@
-import 'package:sqflite/sqflite.dart';
-
 import '../../../model/user.dart';
 
 abstract class DatabaseService {
-  Future<Database> get database;
-  Future<List<User>> getAllUsers();
   Future<void> addUser(User user);
   Future<void> deleteUser(User user);
   Future<void> updateUser(User user);
--- lib/data/source/local/database/database_service_impl.dart
+++ lib/data/source/local/database/database_service_impl.dart
@@ -1,26 +1,23 @@
-import 'package:sqflite/sqflite.dart';
-
 import '../../../model/user.dart';
 import 'database_helper.dart';
 import 'database_service.dart';
 
 class DatabaseServiceImpl implements DatabaseService {
-  // make this class singleton
   DatabaseServiceImpl._();
   static final DatabaseServiceImpl _instance = DatabaseServiceImpl._();
   static DatabaseServiceImpl get instance => _instance;
 
   final DatabaseHelper _databaseHelper = DatabaseHelper.instance;
 
-  @override
-  Future<Database> get database async {
-    throw UnimplementedError();
-  }
+  // @override
+  // Future<Database> get database async {
+  //   throw UnimplementedError();
+  // }
 
-  @override
-  Future<List<User>> getAllUsers() async {
-    throw UnimplementedError();
-  }
+  // @override
+  // Future<List<User>> getAllUsers() async {
+  //   throw UnimplementedError();
+  // }
 
   @override
   Future<void> addUser(User user) async {
